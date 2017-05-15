3099bd9b204bcaac604153bd945ba7a6f1a1fd2e
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-05-15 14:17:50 +0700

    delete ship when remove shipline

--- app/controllers/shipline.controller.js
+++ app/controllers/shipline.controller.js
@@ -79,10 +79,10 @@ exports.insert = function (req, res) {
     var r = req.r;
     var result = { result: false, message: null, id: null };
     if (valid) {
-        req.body = Object.assign(req.body, { 
-            creater : 'admin',
-            date_created : new Date().toISOString(),
-        date_updated : new Date().toISOString(),
+        req.body = Object.assign(req.body, {
+            creater: 'admin',
+            date_created: new Date().toISOString(),
+            date_updated: new Date().toISOString(),
         });
         r.db("common").table("shipline")
             .insert(req.body)
@@ -109,9 +109,9 @@ exports.update = function (req, res) {
     var result = { result: false, message: null, id: null };
     if (req.body.id != '' && req.body.id != null && typeof req.body.id != 'undefined') {
         result.id = req.body.id;
-        req.body = Object.assign(req.body, { 
-            updater : 'admin',
-            date_updated : new Date().toISOString()
+        req.body = Object.assign(req.body, {
+            updater: 'admin',
+            date_updated: new Date().toISOString()
         });
         r.db("common").table("shipline")
             .get(req.body.id)
@@ -141,6 +141,9 @@ exports.delete = function (req, res) {
         r.db('common').table('shipline')
             .get(req.params.id)
             .delete()
+            .do(function (del_do) {
+                return r.db('common').table('ship').getAll(req.params.id, { index: 'shipline_id' }).delete()
+            })
             .run()
             .then(function (response) {
                 result.message = response;
