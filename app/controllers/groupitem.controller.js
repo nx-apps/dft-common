c5e4bb94c4c99a99bd7fcb9de9977c37ceeaea98
Nutthawut-Ice <moomookingza@gmail.com>
2017-07-25 10:16:38 +0700

    close header access-control-allow

--- app/controllers/groupitem.controller.js
+++ app/controllers/groupitem.controller.js
@@ -22,12 +22,12 @@ exports.list = function (req, res) {
         .orderBy(orderby || 'group_name_th')
         .run()
         .then(function (result) {
-            res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3001');
+            // res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3001');
             // res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3003');
             res.json(result)
         })
         .error(function (err) {
-            res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3001');
+            // res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3001');
             // res.setHeader('Access-Control-Allow-Origin', 'https://localhosyt:3003');
             res.json(err)
         })
